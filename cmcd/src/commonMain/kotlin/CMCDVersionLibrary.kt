48fba19e5e7cffbd431947cf599df4987e8024a8
Mostafa Ashraf <mostafa.ashraf@realeyes.com>
2020-10-29 20:31:42 +0200

    CMCD-20 modify class

--- cmcd/src/commonMain/kotlin/CMCDVersionLibrary.kt
+++ cmcd/src/commonMain/kotlin/CMCDVersionLibrary.kt
@@ -1,35 +1,31 @@
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
-
 // NOTE (ben.toofer@realeyes.com): Look into using the delagate pattern
 // https://proandroiddev.com/dont-reinvent-the-wheel-delegate-it-eac132f2aa64
 // https://kotlinlang.org/docs/reference/delegated-properties.html
 // This was a suggestion form the kotlin slack channel
 
-sealed class VersionKeys {
-    enum class Version1Keys {
-        bl,     // Encoded bitrate
-        br,     // Buffer length
-        bs,     // Buffer starvation
-        cid,    // Content ID
-        d,      // Object duration
-        dl,     // Deadline
-        mtp,    // Measured throughput
-        nor,    // Next object request
-        nrr,    // Next range request
-        ot,     // Object type
-        pr,     // Playback rate
-        rtp,    // Requested maximum throughput
-        sf,     // Streaming format
-        sid,    // Session ID
-        st,     // Stream type
-        su,     // Startup
-        tb,     // Top bitrate
-        v       // Version
-    }
-    enum class Version2Keys {
-        temp
-    }
+enum class Version1Keys {
+    bl,     // Encoded bitrate
+    br,     // Buffer length
+    bs,     // Buffer starvation
+    cid,    // Content ID
+    d,      // Object duration
+    dl,     // Deadline
+    mtp,    // Measured throughput
+    nor,    // Next object request
+    nrr,    // Next range request
+    ot,     // Object type
+    pr,     // Playback rate
+    rtp,    // Requested maximum throughput
+    sf,     // Streaming format
+    sid,    // Session ID
+    st,     // Stream type
+    su,     // Startup
+    tb,     // Top bitrate
+    v       // Version
+}
+
+enum class Version2Keys {
+    temp
 }
 
 enum class CMCDHeaderName {
@@ -63,53 +59,32 @@ enum class CMCDStreamType {
     l       // LIVE
 }
 
+data class CMCDProperty<T>(val header: CMCDHeaderName, val description: String, var value: T) {}
 
+abstract class Version(var map: MutableMap<String, CMCDProperty<*>>)
 
-
-/**
- * Manager<V : VersionKeys> {
- *  setProperty(key: V, value: T[V])
- *  getProperty(key: V): T[V]
- * }
- */
-//expect object CMCDLibraryFactory {
-//    fun createCMCDManager(): CMCDManager
-//}
-// Manager<Version1Keys>()
-// manager.setProperty(Version1Keys.bl, 5)
-// manager.setProperty(Version1Keys.cid, 6)
-
-// var manager = Manager<Version2>()
-// manager.setProperty(Version2Keys.bs, "Hello")
-
-data class CMCDProperty<T>( val header: CMCDHeaderName, val description: String, var value: T) {}
-
-abstract class Version<C:IVersionKeys>(map : Map<C, CMCDProperty<*>>)
-
-typealias Version1Structure = Map<IVersionKeys, CMCDProperty<*>>
-typealias Version2Structure = Map<IVersionKeys, CMCDProperty<*>>
-
-class Version1(map: Version1Structure): Version<IVersionKeys>(map) {
-    var br: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.br) as? CMCDProperty<Int>
-    var bl: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.br) as? CMCDProperty<Int>
-    var bs: CMCDProperty<Boolean>? = map.get(VersionKeys.Version1Keys.bs) as? CMCDProperty<Boolean>
-    var cid: CMCDProperty<String>? = map.get(VersionKeys.Version1Keys.cid) as? CMCDProperty<String>
-    var d: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.d) as? CMCDProperty<Int>
-    var dl: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.dl) as? CMCDProperty<Int>
-    var mtp: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.mtp) as? CMCDProperty<Int>
-    var nor: CMCDProperty<String>? = map.get(VersionKeys.Version1Keys.nor) as? CMCDProperty<String>
-    var ot: CMCDProperty<CMCDObjectType>? = map.get(VersionKeys.Version1Keys.ot) as? CMCDProperty<CMCDObjectType>
-    var pr: CMCDProperty<Float>? = map.get(VersionKeys.Version1Keys.pr) as? CMCDProperty<Float>
-    var rtp: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.rtp) as? CMCDProperty<Int>
-    var sf: CMCDProperty<CMCDStreamingFormat>? = map.get(VersionKeys.Version1Keys.sf) as? CMCDProperty<CMCDStreamingFormat>
-    var sid: CMCDProperty<String>? = map.get(VersionKeys.Version1Keys.sid) as? CMCDProperty<String>
-    var st: CMCDProperty<CMCDStreamType>? = map.get(VersionKeys.Version1Keys.st) as? CMCDProperty<CMCDStreamType>
-    var su: CMCDProperty<Boolean>? = map.get(VersionKeys.Version1Keys.su) as? CMCDProperty<Boolean>
-    var tb: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.tb) as? CMCDProperty<Int>
-    var v: CMCDProperty<Int>? = map.get(VersionKeys.Version1Keys.v) as? CMCDProperty<Int>
+class Version1(map: MutableMap<String, CMCDProperty<*>>) : Version(map) {
+    var br: CMCDProperty<Int>? = map[Version1Keys.br.name] as CMCDProperty<Int>
+    var bl: CMCDProperty<Int>? = map[Version1Keys.bl.name] as? CMCDProperty<Int>
+    var bs: CMCDProperty<Boolean>? = map[Version1Keys.bs.name] as? CMCDProperty<Boolean>
+    var cid: CMCDProperty<String>? = map[Version1Keys.cid.name] as? CMCDProperty<String>
+    var d: CMCDProperty<Int>? = map[Version1Keys.d.name] as? CMCDProperty<Int>
+    var dl: CMCDProperty<Int>? = map[Version1Keys.dl.name] as? CMCDProperty<Int>
+    var mtp: CMCDProperty<Int>? = map[Version1Keys.mtp.name] as? CMCDProperty<Int>
+    var nor: CMCDProperty<String>? = map[Version1Keys.nor.name] as? CMCDProperty<String>
+    var ot: CMCDProperty<CMCDObjectType>? = map[Version1Keys.ot.name] as? CMCDProperty<CMCDObjectType>
+    var pr: CMCDProperty<Float>? = map[Version1Keys.pr.name] as? CMCDProperty<Float>
+    var rtp: CMCDProperty<Int>? = map[Version1Keys.rtp.name] as? CMCDProperty<Int>
+    var sf: CMCDProperty<CMCDStreamingFormat>? =
+        map[Version1Keys.sf.name] as? CMCDProperty<CMCDStreamingFormat>
+    var sid: CMCDProperty<String>? = map[Version1Keys.sid.name] as? CMCDProperty<String>
+    var st: CMCDProperty<CMCDStreamType>? = map[Version1Keys.st.name] as? CMCDProperty<CMCDStreamType>
+    var su: CMCDProperty<Boolean>? = map[Version1Keys.su.name] as? CMCDProperty<Boolean>
+    var tb: CMCDProperty<Int>? = map[Version1Keys.tb.name] as? CMCDProperty<Int>
+    var v: CMCDProperty<Int>? = map[Version1Keys.v.name] as? CMCDProperty<Int>
 }
 
-class Version2(map: Version2Structure): Version<IVersionKeys>(map) {
+class Version2(map: MutableMap<String, CMCDProperty<*>>) : Version(map) {
 
 }
 
@@ -118,115 +93,51 @@ enum class VersionLibrary {
     VERSION_2,
     VERSION_3,
 }
-interface IVersionKeys
-interface IVersion1Keys: IVersionKeys
-sealed class V1 {
-    enum class IntKeys: IVersion1Keys {
-        TEMP,
-        SOME
+
+class CMCDManagerTest(val version: Version) {
+    fun setBitrate(key: String, value: Int) {
+        setProperty(key, value)
     }
 
-    enum class StringKeys: IVersion1Keys {
-        WHATEVER
-    }
-}
-
-
-
-class CMCDManagerTest<C : IVersionKeys> {
-
-    val version: Version<C>
-
-    constructor(version: Version<C>) {
-        this.version = version
+    fun setBufferLength(key: String, value: Int) {
+        setProperty(key, value)
     }
 
-    fun setProperty(key: IntKeys, value: Int) {
-        this.version
-    }
+    fun getQueryParams()=version.map
 
-    fun setProperty(key: StringKeys, value: String) {
-        this.version
+    private fun <T> setProperty(key: String, value: T) {
+        val cmcdProperty = version.map[key]
+        val updatedCMCDProperty = cmcdProperty?.header?.let { CMCDProperty(it, cmcdProperty.description, value) }
+        if (updatedCMCDProperty != null) {
+            version.map.put(key, updatedCMCDProperty)
+        }
     }
 }
 
 class CMCDManagerFactoryFake {
-    fun <C: IVersionKeys>createCMCDManager(versionLibrary: VersionLibrary): CMCDManagerTest<C> {
-        var versionObj: Version<*>
-        when(versionLibrary){
-            VersionLibrary.VERSION_1 -> versionObj = Version1(mapOf(
-                IntKeys.SOME to CMCDProperty<Int>(CMCDHeaderName.Object, "", 5),
-                IntKeys.TEMP to CMCDProperty<Int>(CMCDHeaderName.Object, "", 5),
-                StringKeys.WHATEVER to CMCDProperty<Int>(CMCDHeaderName.Object, "", 5),
-                StringKeys.WHATEVER to CMCDProperty<Int>(CMCDHeaderName.Object, "", 5),
-            ))
-            VersionLibrary.VERSION_2 -> versionObj = Version2(emptyMap())
-            else -> versionObj = Version1(emptyMap())
+    fun createCMCDManager(versionLibrary: VersionLibrary): CMCDManagerTest {
+        val versionObj: Version = when (versionLibrary) {
+            VersionLibrary.VERSION_1 -> Version1(
+                mutableMapOf(
+                    Version1Keys.br.name to CMCDProperty(CMCDHeaderName.Object, "", 256),
+                    Version1Keys.bl.name to CMCDProperty(CMCDHeaderName.Object, "", 5),
+                    Version1Keys.d.name to CMCDProperty(CMCDHeaderName.Object, "", 10),
+                )
+            )
+
+            VersionLibrary.VERSION_2 -> Version2(mutableMapOf())
+            else -> Version1(mutableMapOf())
         }
 
-        return CMCDManagerTest<C>(versionObj as Version<C>)
+        return CMCDManagerTest(versionObj)
     }
 }
 
-
-
+@ExperimentalStdlibApi
 fun testing() {
-    var ben = Ben()
-    ben.
-//    var temp: Enum<VersionKeys.Version1Keys> = setOf(VersionKeys.Version1Keys.br, VersionKeys.Version1Keys.bl)
     val factoryFake = CMCDManagerFactoryFake()
-    val manager = factoryFake.createCMCDManager<IVersion1Keys>(VersionLibrary.VERSION_1)
-    manager.setProperty(IntKeys.SOME, 5)
-    manager.setProperty(StringKeys.WHATEVER, "")
+    val manager = factoryFake.createCMCDManager(VersionLibrary.VERSION_1)
+    manager.setBitrate(Version1Keys.br.name, 1)
+    manager.setBufferLength(Version1Keys.bl.name, 5)
 }
-
-
-/**
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-data class Version1_TEMP (
-    val map: Map<String, CMCDProperty<Any>>
-) {
-    fun getValue(key: String): CMCDProperty<Any>? {
-        // nullable and untyped
-        return map[key]
-    }
-}
-
-enum class CMCDKey {
-    BR_KEY, BL_KEY, BS_KEY, CID_KEY
-}
-data class Version3 (
-    val keyProperties: List<CMCDKeyProperty<Any>>
-) {
-    fun <T : Any> getValue(key: CMCDTypedKey<T>): CMCDProperty<T>? {
-        // still nullable but typed...
-        // and the when is exhaustive without an else...
-        return keyProperties.find { key == it.key } as? CMCDProperty<T>
-    }
-}
-sealed class CMCDTypedKey<T : Any> {
-    object BR_KEY : CMCDTypedKey<Int>()
-    object BL_KEY : CMCDTypedKey<Int>()
-    object BS_KEY : CMCDTypedKey<Boolean>()
-    object CID_KEY : CMCDTypedKey<String>()
-}
-sealed class CMCDKeyProperty<T : Any> {
-    abstract val key: CMCDTypedKey<T>
-    abstract val property : T
-}
-class CMCDKeyIntProperty(override val key: CMCDTypedKey<Int>, override val property: Int) : CMCDKeyProperty<Int>()
-class CMCDKeyBoolProperty(override val key: CMCDTypedKey<Boolean>, override val property: Boolean) : CMCDKeyProperty<Boolean>()
-class CMCDKeyStringProperty(override val key: CMCDTypedKey<String>, override val property: String) : CMCDKeyProperty<String>()
 
--- cmcd/src/commonTest/kotlin/CMCDManagerTests.kt
+++ cmcd/src/commonTest/kotlin/CMCDManagerTests.kt
@@ -7,4 +7,15 @@ class CMCDManagerTests {
         val manager = CMCDManagerFactory.createCMCDManager()
         assertEquals("hello!", manager.hello)
     }
+
+    @Test
+    fun createVersionLibrary() {
+        val mockFactory = CMCDManagerFactoryFake()
+        val manager = mockFactory.createCMCDManager(VersionLibrary.VERSION_1)
+        manager.setBitrate(Version1Keys.br.name, 512)
+        val actual = manager.version.map["br"]?.value
+        val expected = 512
+        val expectedQuery=manager.getQueryParams().toString()
+        assertEquals(expected, actual)
+    }
 }
