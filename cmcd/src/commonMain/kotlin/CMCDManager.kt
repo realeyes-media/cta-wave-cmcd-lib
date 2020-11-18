cca6113e422f647891a380a78cf6ecfb888ddb09
John G. Gainfort, Jr <john@realeyes.com>
2020-11-18 15:40:50 -0700

    refactor: common manager lib and initial tests are in a good sport

--- cmcd/src/commonMain/kotlin/CMCDManager.kt
+++ cmcd/src/commonMain/kotlin/CMCDManager.kt
@@ -1,7 +1,13 @@
 interface CMCDManager {
-    val hello: String
+    fun <T> setProperty(key: CMCDPayloadKey, value: T)
+    fun appendQueryParamsToUrl(url: String, objectType: CMCDObjectType, startup: Boolean = false): String
+    fun validate(queryParams: String): Boolean
 }
 
 expect object CMCDManagerFactory {
-    fun createCMCDManager(): CMCDManager
+    fun createCMCDManager(contentId: String, sessionId: String, streamingFormat: CMCDStreamingFormat, version: CMCDVersion = CMCDVersion.VERSION_1): CMCDManager
+}
+
+expect object CMCDUrlEncoder {
+    fun encode(str: String): String
 }
